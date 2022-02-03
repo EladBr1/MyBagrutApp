1e46eaf816f059ded07f1cd835285ba9ac9e1bb5
student <elad.brand1@gmail.com>
2022-02-03 18:42:59 +0200

    first

--- app/src/main/java/com/example/mybagrutapp/Player.java
+++ app/src/main/java/com/example/mybagrutapp/Player.java
@@ -1,8 +1,30 @@
 package com.example.mybagrutapp;
 
+import android.graphics.Bitmap;
+
 public class Player
 {
 
-    private String name;
+    private String tName;
+    private Bitmap profilePic;
+    private String fName;
+    private String birthday;
+    private double height;
+    private String pos;
+    private String crTeam;
+    private int num;
+    private String nltTeam;
+    private int goals;
+    private int asists;
+    private int ntlGoals;
+    private String formerTeams;
+    private String basicInfo;
+
+    public Player( String tName, Bitmap profilePic, String fName, String birthday, double height, String pos, String crTeam, int num, String nltTeam, int goals, int asists, int ntlGoals, String formerTeams, String basicInfo )
+    {
+
+
+
+    }
 
 }
