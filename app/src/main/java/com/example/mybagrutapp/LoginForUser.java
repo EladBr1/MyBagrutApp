c422b2b022c61168d2212e8fe742c0c7aa165846
EladBr1 <elad.brand1@gmail.com>
2022-04-06 11:01:37 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/java/com/example/mybagrutapp/Player.java
    #	build.gradle

--- app/src/main/java/com/example/mybagrutapp/LoginForUser.java
+++ app/src/main/java/com/example/mybagrutapp/LoginForUser.java
@@ -1,11 +1,16 @@
 package com.example.mybagrutapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginForUser extends AppCompatActivity
-{
+public class LoginForUser extends AppCompatActivity {
+
+    private Button loginBtn, reginBtn;
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,7 +18,12 @@ public class LoginForUser extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        username = findViewById(R.id.edUserN);
+        password = findViewById(R.id.edPass);
+        loginBtn = findViewById(R.id.submitBtn);
+        reginBtn = findViewById(R.id.reginBtn);
 
     }
+
+
 }
